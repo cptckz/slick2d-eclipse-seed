6f2314b2777bb724f1256fc434e2e4a2c8ded740
TobiasFThomsen <tobiasthomsen@gmail.com>
2015-05-04 12:59:24 +0200

    her er der lidt nyt

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -13,6 +13,7 @@ import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Vector2f;
+
 	
 
 
@@ -24,15 +25,14 @@ public class SimpleSlickGame extends BasicGame
 	float w = 100;
 	float h = 100;
 	float speed = 20;
-    Image player = null;
-    Image player2 = null;
-    Image player3 = null;
     Image door2 = null;
     Image background2 = null;
     public static int Wscreen = 1240;
     public static int Hscreen = 480;
     public int counter = 0;
-    
+    public int num = 0;
+    public boolean IDLE = true;
+    public playerbaby player = new playerbaby();
     Vector2f trans = new Vector2f(0, 0);
 
 
@@ -45,9 +45,7 @@ public class SimpleSlickGame extends BasicGame
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-		player = new Image("Rec/man2.png");
-		player2 = new Image("Rec/man3.png");
-		player3 = new Image("Rec/man1.png");	
+	
 		background2 = new Image("Rec/grunge-tileset2.png");
 		door2 = new Image("Rec/grunge-tileset-door2.png");
 		gc.setTargetFrameRate(24);
@@ -57,38 +55,43 @@ public class SimpleSlickGame extends BasicGame
 	
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {
+//		
+//		if(gc.getInput().isKeyDown(Input.KEY_A)){
+//		posX-=10;
+//		IDLE = false;
+//		}else if(gc.getInput().isKeyDown(Input.KEY_D)){
+//			posX+=20;
+//			IDLE = false;
+//			}else{
+//				IDLE = true;
+//			}
+//	
+//		if(gc.getInput().isKeyPressed(Input.KEY_W)){
+//			posY-=20;
+//		}
+//		counter++;
 		
-		if(gc.getInput().isKeyDown(Input.KEY_A)){
-		posX-=10;
-		}
-		
-		if(gc.getInput().isKeyDown(Input.KEY_D)){
-			posX+=20;
-			}
-		
-		if(gc.getInput().isKeyPressed(Input.KEY_W)){
-			posY-=20;
+		player.update();
+	 	}
 
-			}
-		counter++;
 
-	}
-	
 
 
 	
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-        background2.draw(0, 0, 1728 , Hscreen);
+		player.draw();
+        background2.draw(0, 0, 1728 , Hscreen); 
+//        if(posX >900 && posX < 1200)
+//        door2.draw(0, 0, 1728 , Hscreen);
+//        if(gc.getInput().isKeyDown(Input.KEY_D)&&counter%4==0){
+//        	player2.draw(posX, posY);
+//        	
+//        }
         
-        if(posX >900 && posX < 1200)
-        door2.draw(0, 0, 1728 , Hscreen);
-        if(gc.getInput().isKeyDown(Input.KEY_D)&&counter%4==0){
-        	player2.draw(posX, posY);
-        }
-        player.draw(posX, posY);
 	}
+   
 	public static void main(String[] args)
 	{
 		try
